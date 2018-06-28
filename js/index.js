1959d7dcfcdf869f72f536f6638984206effdec7
ltz161 <1617567859@qq.com>
2018-06-28 10:23:38 +0800

    提交文件

---
+++ js/index.js
@@ -0,0 +1,28 @@
+var minSize=5;
+var maxSize=50;
+var newOn=150;
+var xueColor="#FFF";
+var xue=$("<div></div>").css({"position":"absolute","top":"-50px"}).html("❄");
+//var xue=$("<div></div>").css({"position":"absolute","top":"-50px"}).html("<img src='img/100.png' style='width:100px;' />");
+$(function(){
+	var dH=$(document).height();
+	var dW=$(document).width();
+	setInterval(function(){
+		var startLeft=Math.random()*dW;
+		var startTM=0.7+Math.random()*0.5;
+		var endTop=dH;
+		var endLeft=Math.random()*dW;
+		var durationFall=4000+Math.random()*3000;
+		var sizexue=minSize+Math.random()*maxSize;
+		xue.clone().appendTo("body").css({
+			"left":startLeft,
+			"opacity":startTM,
+			"font-size":sizexue,
+			"color":xueColor
+		}).animate({
+			"top":endTop,
+			"left":endLeft,
+			"opacity":0.5
+		},durationFall,function(){$(this).remove();});
+	},newOn)
+})
